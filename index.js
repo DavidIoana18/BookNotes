92945b143378c247123c01981631a3a90ba19e5b
David Ioana <dvdioana@gmail.com>
2025-03-04 23:09:14 +0100

    Add a page to view in detail the books read by followers/following

--- index.js
+++ index.js
@@ -200,6 +200,23 @@ app.get("/follower/books/:id", async(req, res) =>{
     }
 });
 
+app.get("/follower/bookDetails/:bookId/:userId", async(req, res) =>{
+    const bookId = req.params.bookId;
+    const userId = req.params.userId;
+    try{
+        const result = await db.query("SELECT * FROM books WHERE id = $1", [bookId]);
+        if(result.rows.length === 0){
+            return res.redirect(`/follower/books/:${userId}`)
+        }else{
+            const book = result.rows[0];
+            res.render("followerBookDetails.ejs", {book: book, userId: userId});
+        }
+    }catch(err){
+        console.log("Error fetching book details: ", err);
+        res.redirect("/user/profile");
+    }
+});
+
 app.get("/user/searchBookCover", (req, res) =>{
     res.render("searchCover.ejs");
 });
